2fd27ddcad968f624c931ce2288a47b722dcc27e
NatPstrag <nati.pstrag@gmail.com>
2022-03-21 11:25:45 +0100

    Initial commit

---
+++ src/main/java/com/example/zgadnijmojaliczbe/ZgadnijMojaLiczbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zgadnijmojaliczbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgadnijMojaLiczbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZgadnijMojaLiczbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zgadnijmojaliczbe/ZgadnijMojaLiczbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zgadnijmojaliczbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZgadnijMojaLiczbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
